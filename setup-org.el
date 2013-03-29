5cba1d6b2a39d2b14bac8c4950212de04be0eb7a
Jonas Hoersch <coroa@online.de>
2013-03-29 17:58:33 +0100

    make the emacs help find the org-manual

--- setup-org.el
+++ setup-org.el
@@ -11,4 +11,17 @@
 (defadvice kill-whole-line (after fix-cookies activate)
   (myorg-update-parent-cookie))
 
+;; Ensure the Latest Org-mode manual is in the info directory
+(if (boundp 'Info-directory-list)
+    (setq Info-directory-list (append Info-directory-list
+                                      Info-default-directory-list))
+  (setq Info-directory-list Info-default-directory-list))
+(setq Info-directory-list
+      (cons (expand-file-name
+             "doc"
+             (expand-file-name
+              "org-mode"
+              (expand-file-name "site-lisp" user-emacs-directory)))
+            Info-directory-list))
+
 (provide 'setup-org)
