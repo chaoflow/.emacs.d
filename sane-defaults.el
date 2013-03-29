7254072130def58c79bea6cac6101fe8d827e46e
Jonas Hoersch <coroa@online.de>
2013-03-29 17:57:03 +0100

    nicer frame-title and show-paren-mode in sane-defaults

--- sane-defaults.el
+++ sane-defaults.el
@@ -34,6 +34,9 @@
 (set-selection-coding-system 'utf-8) ; please
 (prefer-coding-system 'utf-8) ; with sugar on top
 
+;; Frame titles which are short and informative
+(setq frame-title-format "%b - Emacs")
+
 ;; Show active region
 (transient-mark-mode 1)
 (make-variable-buffer-local 'transient-mark-mode)
@@ -56,6 +59,9 @@
 ;; Save a list of recent files visited. (open recent file with C-x f)
 (recentf-mode 1)
 (setq recentf-max-saved-items 100) ;; just 20 is too recent
+
+;; Highlight matching parentheses when the point is on them.
+(show-paren-mode 1)
 
 ;; Undo/redo window configuration with C-c <left>/<right>
 (winner-mode 1)
