22d1994461ba8ce88c9a21074ea6e03171d85391
Jonas Hoersch <coroa@online.de>
2013-03-30 11:32:38 +0100

    minimal tidying

--- init.el
+++ init.el
@@ -35,8 +35,7 @@
 
 ;; Write backup files to own directory
 (setq backup-directory-alist
-      `(("." . ,(expand-file-name
-                 (concat user-emacs-directory "backups")))))
+      `(("." . ,(expand-file-name "backups" user-emacs-directory))))
 
 ;; Make backups of files, even when they're in version control
 (setq vc-make-backup-files t)
