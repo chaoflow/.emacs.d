815fb3536a6c6638e2bb714d4c0b7b36a79cf6b8
Jonas Hoersch <coroa@online.de>
2013-03-31 16:59:11 +0200

    bring over coroas old custom.el

---
+++ users/coroa/custom.el
@@ -0,0 +1,39 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-fold-env-spec-list (quote (("[comment]" ("comment")) ("[figure]" ("figure")))))
+ '(TeX-view-program-list (quote (("okular" "okular %o"))))
+ '(TeX-view-program-selection (quote (((output-dvi style-pstricks) "dvips and gv") (output-dvi "okular") (output-pdf "okular") (output-html "xdg-open"))))
+ '(canlock-password "ad40ab084924ec03dd519cc7cd52a90afa028ae8")
+ '(dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$\\|^\\.")
+ '(dta-default-cfg "default.conf")
+ '(font-use-system-font t)
+ '(global-hl-line-mode t)
+ '(menu-bar-mode nil)
+ '(mm-text-html-renderer (quote gnus-w3m))
+ '(mm-verify-option (quote known))
+ '(mml2015-encrypt-to-self t)
+ '(notmuch-hello-sections (quote (notmuch-hello-insert-header notmuch-hello-insert-saved-searches notmuch-hello-insert-recent-searches notmuch-hello-insert-alltags notmuch-hello-insert-footer)))
+ '(notmuch-saved-searches (quote (("inbox" . "tag:inbox") ("unread" . "tag:unread") ("flagged" . "tag:flagged"))))
+ '(org-agenda-files (quote ("~/notes/diplom.org" "~/notes/reisen.org" "~/notes/qmio-gcalendar.org" "~/notes/goodtoknow.org" "~/notes/termine.org" "~/notes/offizielles.org" "~/notes/foodcoop.org" "~/notes/todo.org")))
+ '(org-file-apps (quote ((auto-mode . emacs) ("\\.mm\\'" . default) ("\\.x?html?\\'" . default) ("\\.pdf::\\(\\d\\)\\'" . "okular -p %1 %s") ("\\.pdf\\'" . "okular %s") ("\\.xoj\\'" . "xournal %s"))))
+ '(org-stuck-projects (quote ("+LEVEL=2/-DONE" ("TODO" "NEXT" "NEXTACTION") nil "Archiv")))
+ '(py-shell-name "python2")
+ '(safe-local-variable-values (quote ((LaTeX-verbatim-environments-local quote ("hcode" "code"))))))
+
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(default ((t (:stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 112 :width normal :foundry "unknown" :family "Terminus (TTF)")))))
+
+ ;; '(default ((t (:stipple nil :background "#2d3743" :foreground "#e1e1e0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 112 :width normal :foundry "unknown" :family "Terminus (TTF)")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
