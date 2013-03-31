5d21178a2fe0c4a1d3bfe0d39e0004186fcccb72
Jonas Hoersch <coroa@online.de>
2013-03-31 17:52:29 +0200

    remove unneeded reference to cl
    defun* would allow extended processing of arguments, which is not needed here

--- defuns/buffer-defuns.el
+++ defuns/buffer-defuns.el
@@ -8,7 +8,7 @@
 (defun mode-keymap (mode-sym)
   (symbol-value (intern (concat (symbol-name mode-sym) "-map"))))
 
-(defun* buffer-local-set-key (key action)
+(defun buffer-local-set-key (key action)
   (when buffer-local-mode
     (define-key (mode-keymap buffer-local-mode)
       key action)
