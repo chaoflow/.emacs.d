6a3783948883e33e8d82d60ede87b2822f39a685
Jonas Hoersch <coroa@online.de>
2013-04-05 17:04:27 +0200

    use buffer-local-variables for project-directory and ipython

--- setup-python.el
+++ setup-python.el
@@ -2,15 +2,6 @@
 
 (add-hook 'python-mode-hook 'whitespace-mode)
 
-;;; virtualenv
-
-(autoload 'virtualenv-activate "virtualenv"
-  "Activate a Virtual Environment specified by PATH" t)
-(autoload 'virtualenv-workon "virtualenv"
-  "Activate a Virtual Environment present using virtualenvwrapper" t)
-
-(define-key python-mode-map (kbd "C-m") 'newline-and-indent)
-
 ;;; flymake
 (when (require 'flymake nil t)
   (require 'flymake-cursor nil t)
@@ -26,19 +17,32 @@
 
   (add-hook 'python-mode-hook 'turn-on-flymake))
 
-;;; ipython
-(let ((ipython (or (executable-find "ipython2")
-                   (executable-find "ipython"))))
-  (when ipython
-    (setq python-shell-interpreter (file-name-nondirectory ipython)
-          python-shell-interpreter-args ""
-          python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-          python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-          python-shell-completion-setup-code
-          "from IPython.core.completerlib import module_completion"
-          python-shell-completion-module-string-code
-          "';'.join(module_completion('''%s'''))\n"
-          python-shell-completion-string-code
-          "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")))
+;;; virtualenv
+
+(autoload 'virtualenv-activate "virtualenv"
+  "Activate a Virtual Environment specified by PATH" t)
+(autoload 'virtualenv-workon "virtualenv"
+  "Activate a Virtual Environment present using virtualenvwrapper" t)
+
+(define-key python-mode-map (kbd "C-m") 'newline-and-indent)
+
+(defun setup-py-buffer ()
+  "Check environment, activate available features"
+
+  (set (make-local-variable 'py-project-directory)
+       (locate-dominating-file buffer-file-name "dev.nix"))
+
+  (when py-project-directory
+    (setq py-project-directory
+          (directory-file-name py-project-directory))
+
+    (make-local-variable 'process-environment)
+    (make-local-variable 'exec-path)
+    (set (make-local-variable 'virtualenv-name) nil)
+    (virtualenv-activate py-project-directory))
+
+  (use-ipython-locally))
+
+(add-hook 'python-mode-hook 'setup-py-buffer)
 
 (provide 'setup-python)
