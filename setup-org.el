chaoflow/.emacs.d

commit f6de3cbe329da581a6bc62725eef484a6d3a4be3

require org explicitly

setup-org.el
@@ -1,3 +1,4 @@
+(require 'org)
 
 (add-to-list 'load-path (concat site-lisp-dir "/org-mode/lisp"))
 

commit 60ffa00068acbce4dc0a17c352a1b932a8ab1b47

fix for regression introduced in f6de3cb
org may only be require'd AFTER the load-path is set up

setup-org.el
@@ -1,6 +1,5 @@
-(require 'org)
-
 (add-to-list 'load-path (concat site-lisp-dir "/org-mode/lisp"))
+(require 'org)
 
 (defun myorg-update-parent-cookie ()
   (when (equal major-mode 'org-mode)