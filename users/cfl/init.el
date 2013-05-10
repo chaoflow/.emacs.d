22527d094f1bbde0b3c9f4f83dbe5b11cacfadaf
Florian Friesdorf <flo@chaoflow.net>
2013-05-10 18:16:02 +0200

    cfl: no currently broken calfw

--- users/cfl/init.el
+++ users/cfl/init.el
@@ -74,7 +74,7 @@
 ;;  )
 
 ;; see https://github.com/kiwanami/emacs-calfw#readme
-(require 'calfw-org)
+;;(require 'calfw-org)
 ;; First day of the week
 (setq calendar-week-start-day 1) ; 0:Sunday, 1:Monday
 (setq cfw:render-line-breaker 'cfw:render-line-breaker-wordwrap)
