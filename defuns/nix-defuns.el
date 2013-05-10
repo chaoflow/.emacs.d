bc06839318d5094fcbf43f30f23814f9a4411eb2
Jonas Hoersch <coroa@online.de>
2013-05-10 09:15:12 +0200

    no error on missing documentation

--- defuns/nix-defuns.el
+++ defuns/nix-defuns.el
@@ -3,6 +3,7 @@
 (defun find-corresponding-nix-hash (file)
   "Given some file or directory which ultimately lies in the nix
 store. Return the corresponding nix hash."
-  (let ((truename (file-truename file)))
-    (when (string-match "/nix/store/\\([^/-]+\\)-" truename)
-      (match-string 1 truename))))
+  (when file
+   (let ((truename (file-truename file)))
+     (when (string-match "/nix/store/\\([^/-]+\\)-" truename)
+       (match-string 1 truename)))))
