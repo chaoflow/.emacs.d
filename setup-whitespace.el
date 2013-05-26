acdb3895b81df35dc5d95fbcc37ca4611baefd58
Jonas Hoersch <coroa@online.de>
2013-05-26 16:15:19 +0200

    remove newline-mark from the default value of whitespace-style
    newline-mark in whitespace-style leads to strange column jumping after
    empty lines and interferes with popup.el, so remove it from the
    default setting

---
+++ setup-whitespace.el
@@ -0,0 +1,7 @@
+;;; newline-mark in whitespace-style leads to strange column jumping
+;;; after empty lines and interferes with popup.el, so remove it from
+;;; the default setting
+
+(cl-callf2 delq 'newline-mark whitespace-style)
+
+(provide 'setup-whitespace)
