63302e62c73c16edbdedabd46e0753e6340a89b1
Jonas Hoersch <coroa@online.de>
2013-06-11 17:21:59 +0200

    Fix ido-ubiquitous confusing completing-read compatibility handling

--- setup-ido.el
+++ setup-ido.el
@@ -49,4 +49,39 @@
 (ido-ubiquitous-use-new-completing-read yas-expand 'yasnippet)
 (ido-ubiquitous-use-new-completing-read yas-visit-snippet-file 'yasnippet)
 
+;; Fix ido-ubiquitous confusing completing-read compatibility handling
+
+(defadvice ido-completing-read (before compensate-quirk activate)
+  "Compensate for a quirk of `completing-read`.
+
+From the `completing-read' docstring:
+
+> If the input is null, `completing-read' returns DEF, or the
+> first element of the list of default values, or an empty string
+> if DEF is nil, regardless of the value of REQUIRE-MATCH.
+
+This odd behavior is required for compatibility with an old-style
+usage pattern whereby the default was requested by returning an
+empty string. In this mode, the caller receives the empty string
+and handles the default case manually, while `completing-read'
+never has any knowledge of the default.
+
+This usage pattern breaks with ido in the edge case when there is
+no DEF, no INITIAL-INPUT, but REQUIRE-MATCH specified, because
+ido then returns the first element on the list rather than the
+empty string. This advice tries then to derive the default item
+from the prompt string by matching against \"(default
+<string>)\" or, if there is no match, it prepends the possible
+choices with the empty string, which will thus be preselected."
+  (when (and ido-next-call-replaces-completing-read
+             (null def)
+             (null initial-input)
+             require-match)
+    (if (string-match "(default \\(\"?\\)\\(.+\\)\\1)" prompt)
+        (setq def (match-string 2 prompt))
+      (push "" choices))))
+
+(setq ido-ubiquitous-enable-compatibility nil)
+
+
 (provide 'setup-ido)
