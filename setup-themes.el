17be3d18e00e0723decf11abbacaff355e04f4cd
Jonas Hoersch <coroa@online.de>
2013-06-16 10:35:54 +0200

    themes: add a reload function and some logic to reload just in time
    im still unsure if it is necessary at all. it seems to work without
    the extra logic most of the time.

--- setup-themes.el
+++ setup-themes.el
@@ -7,6 +7,12 @@ startup."
 (defvar cycle-current-theme nil
   "Used internally to hold a pointer to the currently loaded theme.")
 
+(defun reload-current-theme ()
+  "Reload the theme in `cycle-current-theme', if it is active."
+  (interactive)
+  (when (custom-theme-enabled-p cycle-current-theme)
+    (load-theme cycle-current-theme t)))
+
 (defun cycle-themes ()
   "Cycle through themes from the variable cycle-themes."
   (interactive)
@@ -17,5 +23,9 @@ startup."
        (disable-theme cycle-current-theme))
      (when (load-theme next-theme t)
        (setq cycle-current-theme next-theme)))))
+
+;; (dolist (feature '(magit org-faces whitespace flyspell))
+;;   (unless (featurep feature)
+;;     (eval-after-load feature '(reload-current-theme))))
 
 (provide 'setup-themes)
